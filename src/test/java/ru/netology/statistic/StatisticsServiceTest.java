dd47bdddeb88780665fbcd7e75c7846dffe267a9
uri <katyacitti@gmail.com>
2024-04-11 20:19:05 +0300

    New Test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -12,7 +12,6 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
@@ -22,9 +21,8 @@ public class StatisticsServiceTest {
     void findMaxSecondOpportunity() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 93, 11, 12};
+        long[] incomesInBillions = {12, 5, 51, 4, 5, 3, 8, 6, 93, 11, 12};
         long expected = 93;
-
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
